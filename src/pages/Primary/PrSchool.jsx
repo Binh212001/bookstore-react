f28178380fa2889e2f2788b67b93974748093132
Your Name <you@example.com>
2022-04-23 22:56:15 +0700

    primary school

--- src/pages/Primary/PrSchool.jsx
+++ src/pages/Primary/PrSchool.jsx
@@ -1,7 +1,30 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import Card from "../../Comon/Card";
+import { getCategory } from "../../redux/BookSlice";
 function PrSchool() {
-  return <div className="wapper">PrSchool</div>;
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.Books.productbycategory);
+
+  useEffect(() => {
+    dispatch(getCategory(params));
+  }, [dispatch]);
+
+  return (
+    <div className="wapper">
+      <Container>
+        <Row>
+          {data.map((data, index) => (
+            <Col sm={12} md={6} lg={3} key={index}>
+              <Card data={data} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default PrSchool;
